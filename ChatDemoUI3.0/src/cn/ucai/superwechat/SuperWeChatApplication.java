5a8141a2ec39f4132499a741d1c51cbfd26d2e78
mac-yk <164322105@qq.com>
2016-12-13 22:36:26 +0800

    修复整合中出现的错误，删除demo,application和costant

--- ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatApplication.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatApplication.java
@@ -18,7 +18,9 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 
 import com.easemob.redpacketsdk.RedPacket;
+import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.domain.User;
+import com.ucloud.live.UEasyStreaming;
 
 public class SuperWeChatApplication extends Application {
 
@@ -46,6 +48,8 @@ public class SuperWeChatApplication extends Application {
 		RedPacket.getInstance().initContext(applicationContext);
 		RedPacket.getInstance().setDebugMode(true);
 		//end of red packet code
+		EaseUI.getInstance().init(this, null);
+		UEasyStreaming.initStreaming("publish3-key");
 	}
 
 	public static SuperWeChatApplication getInstance() {
